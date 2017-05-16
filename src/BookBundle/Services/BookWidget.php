0786e3a662419f7c7c34c84a1dc569d2bb6febb6
Marko Shemechko <marko@webcodium.com>
2017-05-16 17:28:07 +0300

    Best reasders slider

---
+++ src/BookBundle/Services/BookWidget.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark
+ * Date: 28.01.16
+ * Time: 1:58
+ */
+
+namespace BookBundle\Services;
+
+use ProfileBundle\Entity\User;
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class BookWidget {
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $service_container;
+
+    /**
+     * @var DataCollectorTranslator
+     */
+    protected $translator;
+
+    /**
+     * @var
+     */
+    protected $em;
+
+    /**
+     * @var User
+     */
+    protected $user;
+
+
+
+    public function __construct(ContainerInterface $container, $em){
+        $this->service_container = $container;
+        $this->translator = $this->service_container->get('translator');
+        $this->em = $em;
+        $this->user = $this->service_container->get('security.context')->getToken();
+        $this->user = $this->user ? $this->user->getUser() : null;
+    }
+
+    public function getBestReaders(){
+        return $this->em->getRepository('ProfileBundle:User')->getBestReaders();
+    }
+
+
+}
--- web/assetic/bootstrap_main_10.js
+++ web/assetic/bootstrap_main_10.js
@@ -165,4 +165,43 @@ jQuery(function ($) {
             $(this).addClass('active');
         }
     });
+
+
+    // Best Readers slider START
+    $('.js-best-readers').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    // Best Readers slider END
 });
