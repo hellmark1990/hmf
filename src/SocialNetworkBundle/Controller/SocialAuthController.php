8a655f33cb9ab3d0df5f6e14cf9f2c206e05282e
Marko Shemechko <marko@webcodium.com>
2017-07-02 13:45:17 +0300

    Social auth

--- src/SocialNetworkBundle/Controller/SocialAuthController.php
+++ src/SocialNetworkBundle/Controller/SocialAuthController.php
@@ -2,6 +2,7 @@
 
 namespace SocialNetworkBundle\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
